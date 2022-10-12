459351917aa6e47908e2e4725cf13123a9113794
Aditya Kanikdaley <adikanikdaley@gmail.com>
2022-10-12 15:53:07 +0530

    removed unused imports, added eslint lines

--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -4,12 +4,8 @@ import PropTypes from 'prop-types';
 import './movie-list.scss';
 
 import { SwiperSlide, Swiper } from 'swiper/react';
-import { Link } from 'react-router-dom';
-
-import Button from '../button/Button';
 
 import tmdbApi, { category } from '../../api/tmdbApi';
-import apiConfig from '../../api/apiConfig';
 
 import MovieCard from '../movie-card/MovieCard';
 
@@ -36,9 +32,11 @@ const MovieList = props => {
             setItems(response.results);
         }
         getList();
+    // eslint-disable-next-line
     }, []);
 
     return (
+        // eslint-disable-next-line
         <div className="movie-list">
             <Swiper
                 grabCursor={true}
